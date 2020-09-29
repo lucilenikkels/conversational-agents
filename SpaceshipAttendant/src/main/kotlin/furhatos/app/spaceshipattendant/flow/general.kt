4b4848a9e7a385b50241b8dc964a0657fa1882b6
Lucile Nikkels <luus.nikkels@gmail.com>
2020-09-29 21:57:48 +0200

    Play with microexpressions

--- SpaceshipAttendant/src/main/kotlin/furhatos/app/spaceshipattendant/flow/general.kt
+++ SpaceshipAttendant/src/main/kotlin/furhatos/app/spaceshipattendant/flow/general.kt
@@ -1,12 +1,26 @@
 package furhatos.app.spaceshipattendant.flow
 
+import furhatos.autobehavior.*
 import furhatos.flow.kotlin.*
+import furhatos.gestures.BasicParams
+import furhatos.gestures.Gestures
 import furhatos.util.*
 
 val Idle: State = state {
 
     init {
         furhat.setVoice(Language.ENGLISH_US, Gender.MALE)
+
+        furhat.setMicroexpression(
+                defineMicroexpression {
+                    // Adjust eye gaze randomly (between -180 and 180 degrees) with a random interval of 0-1000 ms.
+                    repeat(0..1000) {
+                        adjust(-180.0..180.0, BasicParams.GAZE_PAN)
+                        adjust(-180.0..180.0, BasicParams.GAZE_TILT)
+                    }
+                    repeat(2000..8000, Gestures.Blink)
+                })
+
         if (users.count > 0) {
             furhat.attend(users.random)
             goto(InitialState)
